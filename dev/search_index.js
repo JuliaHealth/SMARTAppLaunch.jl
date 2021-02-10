191574266843e76aa7adbc60c3a99ab1417c57ec
Documenter.jl <documenter@juliadocs.github.io>
2021-02-10 05:13:31 +0000

    build based on 27b3802

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SMARTAppLaunch","category":"page"},{"location":"#SMARTAppLaunch","page":"Home","title":"SMARTAppLaunch","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SMARTAppLaunch]","category":"page"}]
+}
