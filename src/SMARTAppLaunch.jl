4fb0957512c3e9f01bb32c2ec5d79327925c9f20
Dilum Aluthge <dilum@aluthge.com>
2021-02-15 08:23:33 -0500

    Add the EHR Launch workflow (a.k.a. the Embedded Launch workflow)

--- src/SMARTAppLaunch.jl
+++ src/SMARTAppLaunch.jl
@@ -5,14 +5,19 @@ import JSON3
 import JSONWebTokens
 import URIs
 
+export ProviderEHRLaunchConfig
 export ProviderStandaloneConfig
 export get_access_token
+export provider_ehr_launch
 export provider_standalone_step1
 export provider_standalone_step2
 
 include("types.jl")
 
+include("code_to_token.jl")
 include("jwt.jl")
-include("provider_standalone.jl")
+
+include("provider/ehr_launch.jl")
+include("provider/standalone.jl")
 
 end # module
