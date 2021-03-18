5d789ca931bab29b32181bde0ff3467ee3a37363
Dilum Aluthge <dilum@aluthge.com>
2021-03-18 07:52:51 -0400

    Implement the HealthBase `get_fhir_access_token` interface (#18)

--- src/SMARTAppLaunch.jl
+++ src/SMARTAppLaunch.jl
@@ -1,13 +1,20 @@
 module SMARTAppLaunch
 
 import HTTP
+import HealthBase
 import JSON3
 import JSONWebTokens
 import URIs
 
+const get_fhir_access_token = HealthBase.get_fhir_access_token
+const get_fhir_patient_id   = HealthBase.get_fhir_patient_id
+const has_fhir_patient_id   = HealthBase.has_fhir_patient_id
+
 export ProviderEHRLaunchConfig
 export ProviderStandaloneConfig
-export get_access_token
+export get_fhir_access_token
+export get_fhir_patient_id
+export has_fhir_patient_id
 export provider_ehr_launch
 export provider_standalone_step1
 export provider_standalone_step2
