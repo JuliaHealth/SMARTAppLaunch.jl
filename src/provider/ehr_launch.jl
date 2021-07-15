fc95e1b4b714eac1d7171a8056c40494ccd82c04
Dilum Aluthge <dilum@aluthge.com>
2021-07-15 00:37:04 -0400

    Throw a better exception (instead of logging an error and throwing a less informative exception) (#26)

--- src/provider/ehr_launch.jl
+++ src/provider/ehr_launch.jl
@@ -84,12 +84,14 @@ function provider_ehr_launch(config::ProviderEHRLaunchConfig;
         redirect = false,
     )
     headers = Dict(authorize_response.headers)
-    auth_error_msg = string(
-        "Something when wrong when authenticating to the EHR. ",
-        "One possible explanation is that your `redirect_uri` value does not exactly ",
-        "match any of the `redirect_uri` values that are on file with the EHR.",
-    )
-    haskey(headers, "Location") || @error(auth_error_msg)
+    let 
+        auth_error_msg = string(
+            "Something when wrong when authenticating to the EHR. ",
+            "One possible explanation is that your `redirect_uri` value does not exactly ",
+            "match any of the `redirect_uri` values that are on file with the EHR.",
+        )
+        haskey(headers, "Location") || throw(ErrorException(auth_error_msg))
+    end
     location = headers["Location"]
     location_uri = URIs.URI(location)
     location_queryparams = URIs.queryparams(location_uri)
